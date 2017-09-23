82f0c3af13b6dd73c11767dcecc1f8f5fc782a29
LianWX <1270739445@qq.com>
2017-09-23 20:00:55 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/entity/VersionEntity.java
@@ -0,0 +1,11 @@
+package cn.edu.gdmec.android.mobileguard.m1home.entity;
+
+/**
+ * Created by admin on 2017/9/22.
+ */
+
+public class VersionEntity {
+    public String versioncode;
+    public String description;
+    public String apkurl;
+}
