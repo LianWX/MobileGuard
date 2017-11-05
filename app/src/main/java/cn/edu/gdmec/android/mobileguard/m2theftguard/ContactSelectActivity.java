3183be10a733a2507bfb3a160bf019e05cd25e7d
LianWX <1270739445@qq.com>
2017-11-05 21:48:10 +0800

    2017/11/5_10

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/ContactSelectActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/ContactSelectActivity.java
@@ -43,6 +43,7 @@ public class ContactSelectActivity extends AppCompatActivity implements View.OnC
         setContentView(R.layout.activity_contact_select);
         initView();
     }
+
     private void initView(){
         ((TextView) findViewById(R.id.tv_title)).setText("选择联系人");
         ImageView mLeftImgv=(ImageView) findViewById(R.id.imgv_leftbtn);
