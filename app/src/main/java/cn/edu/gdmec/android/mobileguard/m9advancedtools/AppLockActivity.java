f8eefa7076327eee171cedb36560d3433bfd8354
LianWX <1270739445@qq.com>
2017-12-23 01:29:33 +0800

    2017/12/22/07

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m9advancedtools/AppLockActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m9advancedtools/AppLockActivity.java
@@ -86,6 +86,7 @@ public class AppLockActivity extends AppCompatActivity implements View.OnClickLi
     }
 
     private void initView() {
+
         findViewById(R.id.rl_titlebar).setBackgroundColor(
                 getResources().getColor(R.color.bright_red));
         ImageView mLeftImgv = (ImageView) findViewById(R.id.imgv_leftbtn);
